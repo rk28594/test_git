7e90a1b073bd4553cbc31c33795ce298f46f772b
raghavendra.konala <raghavendra.konala@techwave.com>
2026-01-19 12:18:20 +0530

    GENAI=YES reverted un necessary files

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,7 +3,7 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         // START GENAI
-        System.out.println("Hello from genai-hook sample");
+        System.out.println("Hello from genai-hook sample"); 
         // END GENAI
     }
 }
